bf9b24eefbd7620f0fbb95f2a934e67227999391
MitheshShettty <shettymithesh030@gmail.com>
2026-06-13 09:29:09 +0530

    Implement selection sorting technique

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,41 @@
+// Selection Sort implementation in C++
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+
+    cout << "Before Sorting: ";
+    for (int x : arr) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr);
+
+    cout << "After Sorting: ";
+    for (int x : arr) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
